122bfbf325dcc08c8436cbb7029d6bddb4436dd1
Vicente de Alencar <vicentedealencar@gmail.com>
2012-09-29 16:33:50 -0300

    database model ready

---
+++ BarDaLuz.Db/Models.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace BarDaLuz.Db
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public bool IsEntrance { get; set; }
+        public virtual List<ProductTab> ProductTab { get; set; }
+    }
+
+    public class Tab
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public bool Paid { get; set; }
+        public virtual List<ProductTab> ProductTab { get; set; }
+    }
+
+    public class ProductTab
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int TabId { get; set; }
+        public virtual Tab Tab { get; set; }
+    }
+}
